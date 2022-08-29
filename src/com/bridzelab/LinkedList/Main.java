7c44371b5e76f65ab0e6058b2941334642b5bdcc
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-08-29 11:27:04 +0530

    [SUSHMITHA]ADDED:created a simple linked list and added 56,30 and 70

---
+++ src/com/bridzelab/LinkedList/Main.java
@@ -0,0 +1,21 @@
+package com.bridzelab.LinkedList;
+
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+	
+			LinkedList list = new LinkedList();    
+		    
+		    //Adding nodes to the list    
+		    list.insert(56);    
+		    list.insert(30);    
+		    list.insert(70);        
+		        
+		    //Displays the nodes present in the list    
+		    list.show();    
+	}   
+}
+
+
---
+++ src/com/bridzelab/LinkedList/Node.java
@@ -0,0 +1,17 @@
+package com.bridzelab.LinkedList;
+
+public class Node 
+{	
+	public int data;
+	public Node next;
+    
+	//Node Constructor for Creating New Node
+	public Node(int data) 
+	{
+		this.data = data;
+		this.next = null;
+	}
+
+		
+
+}
